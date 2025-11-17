c6617da25e2194406c0977529e7ecc6204b093b5
nfawzy <Nfawzy@deloiite.com>
2025-11-17 04:09:34 +0200

    Initial commit

---
+++ src/main/java/com/swiftbus/swiftbus_service/SwiftbusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbus.swiftbus_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftbusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftbusServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftbus/swiftbus_service/SwiftbusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftbus.swiftbus_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftbusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
